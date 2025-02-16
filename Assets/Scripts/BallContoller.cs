0b12434f544f040708ccc3c379d29cf063d9d271
Isaac Jasper <60370374+Isaac-Jasper@users.noreply.github.com>
2025-02-15 18:38:34 -0800

    addedindicator

--- Assets/Scripts/BallContoller.cs
+++ Assets/Scripts/BallContoller.cs
@@ -5,6 +5,7 @@ public class Ball : MonoBehaviour
     [SerializeField] private float force = 1f;
     [SerializeField] private Transform ballAnchor;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform launchIndicator;
     [SerializeField] private Rigidbody rb;
 
     private bool isBallLaunched;
@@ -26,6 +27,7 @@ public class Ball : MonoBehaviour
         isBallLaunched = true;
         transform.parent = null;
         rb.isKinematic = false;
-        rb.AddForce(transform.forward * force, ForceMode.Impulse);
+        rb.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
     }
 }
