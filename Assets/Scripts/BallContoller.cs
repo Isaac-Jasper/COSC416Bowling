1501cf876a9b9f9d0c264237836734d8fb485c66
Isaac Jasper <60370374+Isaac-Jasper@users.noreply.github.com>
2025-02-15 18:32:34 -0800

    implemented ball moving with player

--- Assets/Scripts/BallContoller.cs
+++ Assets/Scripts/BallContoller.cs
@@ -3,20 +3,29 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Rigidbody rb;
 
     private bool isBallLaunched;
 
     void Start() {
+        Init();
+    }
+
+    private void Init() {
         if (rb == null) rb = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        rb.isKinematic = true;
     }
 
     private void LaunchBall() {
         if (isBallLaunched) return;
         isBallLaunched = true;
-        
+        transform.parent = null;
+        rb.isKinematic = false;
         rb.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
